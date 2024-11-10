91bc4d87a6c58edeb026721e18c5ba661151e8c3
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-11-10 03:03:06 +0100

    "Fix views for listing books and displaying library details"

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from .models import Book
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
-
-# relationship_app/views.py
 from django.views.generic import DetailView
 from .models import Library
 
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Query to get all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'  # Template for the library details page
+    context_object_name = 'library'  # Name of the context variable to access the library in the template
