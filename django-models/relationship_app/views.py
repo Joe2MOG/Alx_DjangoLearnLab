93216696b29cf8f65e9a69fd191fe6377f6bd622
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-11-10 22:03:26 +0100

    "Update admin_view"

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -28,15 +28,3 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
-from .models import UserProfile
-
-# Custom check function for the Admin role
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)  # This decorator will check for the admin role
-def admin_view(request):
-    # Your code here for the admin-specific content or page
-    return render(request, 'relationship_app/admin_view.html')
