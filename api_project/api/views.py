b99c1422934477365a67e37d889593ef85a18b56
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-11-23 12:09:45 +0100

    "Update views.py"

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
+import rest_framework  # Import the entire rest_framework module
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(rest_framework.generics.ListAPIView):  # Extend ListAPIView via rest_framework
     queryset = Book.objects.all()  # Retrieve all Book instances
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
