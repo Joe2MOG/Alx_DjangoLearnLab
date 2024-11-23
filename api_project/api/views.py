021d25f699de51a1cc992aee84398daa17aed052
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-11-23 12:22:07 +0100

    "Update rest_framework.generics as generics in views.py"

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-import rest_framework  # Import the entire rest_framework module
+import rest_framework.generics as generics  # Import generics as a module
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(rest_framework.generics.ListAPIView):  # Extend ListAPIView via rest_framework
+class BookList(generics.ListAPIView):  # Use generics to extend ListAPIView
     queryset = Book.objects.all()  # Retrieve all Book instances
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
