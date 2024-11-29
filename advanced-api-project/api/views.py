3559d82d90954a3a69cb6a9aa765a451711020b0
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-11-29 04:36:20 +0100

    "Added permission classes to secure API endpoints"

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,19 +6,19 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]  # Public access
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Apply permission
 
 # DetailView: Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]  # Public access
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Apply permission
 
 # CreateView: Add a new book
 class BookCreateView(generics.CreateAPIView):
