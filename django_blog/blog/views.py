e8122ac750a9f101ffb9a96caf462966c87be306
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-12-04 01:04:46 +0100

    "Add UserPassesTestMixin to post update and delete views"

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,16 @@
 # from django.shortcuts import render
 
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+from django.views.generic import UpdateView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import DeleteView
 
 # Create your views here.
 
@@ -77,3 +84,24 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
 
     success_url = reverse_lazy('blog:post_list')
 
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user  # Ensures the logged-in user is the post author
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+    
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user  # Ensures the logged-in user is the post author
