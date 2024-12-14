bfe1a4b286a9d5628ae767a7e2593d03724b8c2f
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-12-14 02:14:26 +0100

    "Fix: Ensure serializers.py meets checker's requirements"

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,25 +2,27 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
+# Get the user model dynamically
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+    bio = serializers.CharField(required=False, allow_blank=True)
+    profile_picture = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-    
+
     def create(self, validated_data):
-        # Create user and set password properly
-        user = User.objects.create_user(
+        # Create a new user using create_user method
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        # Generate token for the newly created user
+        # Generate a token for the user
         Token.objects.create(user=user)
         return user
-
